df9efe62594ccb7d904942b123b35ea25ec76da6
JosseJuventino <jcastillo@inventa.com.sv>
2025-12-01 15:25:14 -0600

    Actividad clase 3

---
+++ Actividad clase 3/conocimientos.pl
@@ -0,0 +1,85 @@
+% Hijos
+hijo(clara, mariela).
+hijo(rodolfo, mariela).
+hijo(ismael, mariela).
+hijo(lucas, rodolfo).
+hijo(clara, esteban).
+hijo(rodolfo, esteban).
+hijo(ismael, esteban).
+hijo(martina, rodolfo).
+hijo(martina, teresa).
+
+% amigos
+amigo(ismael, hector).
+
+% casados
+casado(esteban, mariela).
+casado(mariela, esteban).
+casado(andres, clara).
+casado(clara, andres).
+casado(teresa, rodolfo).
+casado(rodolfo, teresa).
+casado(catalina, andres).
+casado(andres, catalina).
+casado(alonso, doña_beatriz).
+casado(doña_beatriz, alonso).
+
+% Oficios
+oficio(hector, herrero).
+oficio(teresa, tejedora).
+oficio(clara, maestra).
+oficio(catalina, curandera).
+oficio(felipe, explorador).
+oficio(emilia, ceramista).
+oficio(mateo, agricultor).
+oficio(irene, criadora, vendedora).
+oficio(rodolfo, agricultor).
+oficio(lucas, herrero).
+
+% Rivalidad
+rival(rodolfo, felipe).
+rival(clara, catalina).
+
+% Veneraban cultos
+verenaban(espiritu_agua, rivoria).
+veneraban(espiritu_montaña, montelar).
+veneraban(espiritu_tierra, durania).
+
+% Aldeas
+aldea(rivoria).
+aldea(montelar).
+aldea(durania).
+
+% Personas
+persona(don_esteban).
+persona(doña_maricela).
+persona(clara).
+persona(rodolfo).
+persona(ismael).
+persona(andres).
+persona(sofia).
+persona(teresa).
+persona(lucas).
+persona(martina).
+persona(hector).
+persona(don_alonso).
+persona(doña_beatriz).
+persona(felipe).
+persona(catalina).
+persona(emilia).
+persona(mateo).
+persona(irene).
+
+% Ubicacion
+vive_en(don_esteban, rivoria).
+vive_en(doña_maricela, rivoria).
+vive_en(clara, rivoria).
+vive_en(rodolfo, rivoria).
+vive_en(ismael, rivoria).
+vive_en(hector, rivoria).
+vive_en(teresa, rivoria).
+vive_en(lucas, rivoria).
+
+% Pactos
+pacto(rivoria, durania).
+pacto(durania, rivoria).
